b9cc8f26c5dc14d0848145822e176fe917fcdea2
VikasKumarSingh <vikaskumarsingh0931@gmail.com>
2023-02-10 11:44:41 +0530

    Update add.py

--- Sublime_Main_Folder/add.py
+++ Sublime_Main_Folder/add.py
@@ -1,4 +1,5 @@
+#  This is program of addition which is written in python
 n1=int(input("entr only digit:"))
 n2=int(input("enter 2nd digit:"))
 sum=n1+n2
-print(sum)
+print("sum of two numbers " + sum)
